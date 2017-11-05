35fff95d17de815d0f6b8d3fb7a583acb9b8285e
renuraj18 <renuraj18@gmail.com>
2017-11-05 02:30:49 +0000

    resolved case issue with update proxy

--- updateProxy.sh
+++ updateProxy.sh
@@ -1,7 +1,8 @@
 PROXY=$1
+PROXYU=`echo $PROXY | tr '[:lower:]' '[:upper:]'`
 Mount=`docker volume inspect nginx_nginx_resources --format ''{{.Mountpoint}}''`
 TOOLS_FILE=$Mount/configuration/sites-enabled/tools-context.conf
-if grep -e "###START-PROXY:$PROXY###" -e "###END-PROXY:PROXY:$PROXY###" $TOOLS_FILE; then
+if grep -e "###PROXY:$PROXYU###" -e "###PROXY:$PROXYU###" $TOOLS_FILE; then
 	echo "Proxy Already exists"
 else
 	echo "Adding $PROXY"
